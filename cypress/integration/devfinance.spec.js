93c3d8bd55fe481856dd4176b52dfb5abc98e70e
ligia.bozzi@gmail.com <ligia.bozzi@gmail.com>
2021-11-07 10:52:22 -0300

    refactoring to add onBeforeLoad

--- cypress/integration/devfinance.spec.js
+++ cypress/integration/devfinance.spec.js
@@ -1,14 +1,16 @@
 /// <reference types="cypress" />
 
-import { format } from '../support/utils'
+import { format, prepareLocalStorage } from '../support/utils'
 
 context('DevFinance', () => {
 //executar antes de cada teste 
 beforeEach(()=>{
-    cy.visit('https://devfinance-agilizei.netlify.app/#') 
-    cy.get('#data-type tbody tr').should('have.length', 0 )
+    cy.visit('https://devfinance-agilizei.netlify.app/#',{
+        onBeforeLoad:(win)=>{
+            prepareLocalStorage(win)
+        }
+    })    
 });
-
     it('Adicionar uma nova transação de entrada', () => {
         
         cy.get('#transaction a.button.new').click()
@@ -18,7 +20,7 @@ beforeEach(()=>{
 
         cy.contains('Salvar').click();
 
-        cy.get('#data-table tbody tr').should('have.length',1)
+        cy.get('#data-table tbody tr').should('have.length',3)
     })
 
     it('Adicionar uma nova transação de saída', ()=>{
@@ -30,7 +32,7 @@ beforeEach(()=>{
 
         cy.contains('Salvar').click();
 
-        cy.get('#data-table tbody tr').should('have.length', 1)
+        cy.get('#data-table tbody tr').should('have.length', 3)
     })
     it('Remover uma transação de entrada ou saída', () =>{
         
@@ -41,35 +43,19 @@ beforeEach(()=>{
 
         cy.contains('Salvar').click();
 
-        cy.get('#data-table tbody tr').should('have.length',1)
 
         cy.get('td.description')
-        cy.contains('freela') 
+        cy.contains('Comida') 
         .parent()   
         .find('img[onclick*=remove]')  
         .click()
 
-        cy.get('table tbody tr').should('have.length', 0)
+        cy.get('table tbody tr').should('have.length', 2)
 
     })
 
     it('Validar Saldos com Diversas Transações',() => {
-        cy.get('a[onclick*=open]').click()
-        cy.get('#description').type('freela')
-        cy.get('#amount').type(120)
-        cy.get('#date').type('2021-11-03')
 
-        cy.contains('Salvar').click();
-
-        
-        cy.get('#transaction a.button.new').click()
-        cy.get('#description').type('café')
-        cy.get('#amount').type(-12)
-        cy.get('#date').type('2021-11-03')
-
-        cy.contains('Salvar').click();
-
-        
         let incomes = 0; 
         let expenses = 0
         cy.get('#data-table tbody tr')
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -4,6 +4,25 @@ export const format = (value) => {
     formattedValue = value.replace(',', '.')
     formattedValue = Number(formattedValue.split('$')[1].trim())
 
-    formattedValue = String(value).includes('-') ? -formattedValue: formattedValue
+    formattedValue = String(value).includes('-') ? -formattedValue : formattedValue
     return formattedValue
+}
+
+export const randomNumber = () => {
+    return Math.floor(Math.random() * 101)
+}
+
+export const prepareLocalStorage = (win) => {
+    win.localStorage.setItem('dev.finances:transactions', JSON.stringify([
+        {
+            description: "Mesada",
+            amount: randomNumber() * 100,
+            date: "01/11/2021"
+        },
+        {
+            description: "Comida",
+            amount: - (randomNumber() * 100),
+            date: "07/11/2021"
+        }
+    ]))
 }
